d648ed598b3f33c5e2102cb1d1b308db4ac664ce
Kovidh <kovidhvsbhati@gmail.com>
2025-08-27 18:18:05 +0530

    add initializer class; make usable in DAW

--- app.js
+++ app.js
@@ -35,29 +35,36 @@ class Connection {
     static #outputPort = null;
     static #initialized = false;
 
-    constructor() { }
-
     static async initialize() {
-        if (this.#initialized) {
+        if (Connection.#initialized) {
             return
         }
 
         try {
-            this.#midi = await navigator.requestMIDIAccess();
-            this.#outputPort = [...this.#midi.outputs.values()].find(o => o.name.includes('ProgressionPadPort'));
-            this.#initialized = true;
+            Connection.#midi = await navigator.requestMIDIAccess();
+            Connection.#outputPort = [...Connection.#midi.outputs.values()].find(o => o.name.includes('ProgressionPadPort'));
+            Connection.#initialized = true;
+            alert('Connected to virtual MIDI Port Successfully');
         } catch (err) {
-            console.err("MIDI Initialization failed: ", err);
+            throw new Error(`MIDI Initialization failed: ${err}`)
+            // console.error("MIDI Initialization failed: ", err);
         }
     }
 
     static async getPort() {
         await this.initialize();
-        return this.#outputPort;
+        return Connection.#outputPort;
     }
 
-    static send(status, note, velocity) {
-        this.#outputPort.send([status, note, velocity]);
+    static async send(status, note, velocity) {
+        if (!Connection.#initialized) {
+            await Connection.initialize();
+        }
+
+        if (!Connection.#outputPort) {
+            throw new Error('Connection output not assigned: class Connection{ send(status, note, velocity }');
+        }
+        Connection.#outputPort.send([status, note, velocity]);
     }
 }
 
@@ -65,44 +72,59 @@ class Transmitter {
     static #statusOn = 0x90;
     static #statusOff = 0x80;
     static #velocity = 127;
-    static connection = new Connection();
 
     constructor() {
-        connection.initialize();
+        Connection.initialize().catch((err) => { console.error(err) });
     }
 
-    static play(note) {
-        Transmitter.connection.send(Transmitter.#statusOn, note, Transmitter.#velocity);
+    async play(note, velocity = Transmitter.#velocity) {
+        await Connection.send(Transmitter.#statusOn, note, velocity);
     }
 
-    static stop(note) {
-        Transmitter.connection.send(Transmitter.#statusOff, note, Transmitter.#velocity);
+    async stop(note) {
+        await Connection.send(Transmitter.#statusOff, note, 0);
     }
 }
 
-class Chord {
-    constructor() {
+class ChordGenerator {
+    // Notes for octave = -1 as standard
+    static NOTE_MAP = new Map([
+        ["C", 0],
+        ["C#", 1],
+        ["D", 2],
+        ["D#", 3],
+        ["E", 4],
+        ["F", 5],
+        ["F#", 6],
+        ["G", 7],
+        ["G#", 8],
+        ["A", 9],
+        ["A#", 10],
+        ["B", 11]
+    ]); // midi value = NOTE+12*octave
 
-    }
-
-    static #getChord(root, quality) {
-        const semitones = 12;  // 12 ko 15 se replace krdo agar next octave ke notes consider krne hai for a chord
+    getChord({ root, quality }) {
+        const semitones = 24;  // 24 full range chords
         root = root.toUpperCase();
         quality = quality.toLowerCase();
+        console.log("Root: ", root, " Quality: ", quality);
+        const rootNote = ChordGenerator.NOTE_MAP.get(root);
+        console.log(rootNote);
         switch (quality) {
             case "major":
-                return [NOTE[root], (NOTE[root] + 4) % semitones, (NOTE[root] + 7) % semitones];
+                return [rootNote, (rootNote + 4) % semitones, (rootNote + 7) % semitones];
             case "minor":
-                return [NOTE[root], (NOTE[root] + 3) % semitones, (NOTE[root] + 7) % semitones];
-            case "diminshed":
-                return [];
+                return [rootNote, (rootNote + 3) % semitones, (rootNote + 7) % semitones];
+            case "diminished":
+                return [rootNote, (rootNote + 3) % semitones, (rootNote + 6) % semitones];
             default:
-                return [];
+                return ["default"];
         }
     }
 
-    static #getRootQuality(event) {
-        const cell = event.target;
+    getRootQuality(/*event, */cell) {
+        // const cell = event.target;
+        console.log(cell);
         const root = cell.getAttribute('data-root');
 
         const row = cell.parentNode;
@@ -110,6 +132,95 @@ class Chord {
         const qualityHeaders = document.querySelectorAll('.pad-table thead th');
         const quality = qualityHeaders[cellIndexInRow].getAttribute('data-quality');
 
-        return {root, quality};
+        return { root, quality };
     }
 } // this class resolves the abstraction of chords and notes as String/character to their number representation so that we can leverage Transmitter to commuincate with the Virtual MIDI Port to play those chords and notes 
+
+class Initializer {
+    static chordGenerator = new ChordGenerator();
+    static transmitter = new Transmitter();
+    static octave = 2;
+    static velocity = 127;
+    static ProgressionPads = {
+        major: new Pad({ containerClass: "pads", progression: majorChordProg }),
+        minor: new Pad({ containerClass: "pads", progression: minorChordProg })
+    }
+
+    constructor(padContainerId, octaveInputId, progressionSelectId) {
+        Initializer.initialize(padContainerId, octaveInputId, progressionSelectId);
+    }
+
+    static initialize(padContainerId, octaveInputId, progressionSelectId) {
+        if (typeof padContainerId != "string" || typeof octaveInputId != "string") {
+            throw new Error('Container ID or(and) Octave Input ID must be of type "string" : class Initializer{ static initialize(containerId) }');
+        }
+        //render components on the page
+        this.#renderComponents();
+        //initialize handlers for Controller Elements
+        this.#intializeControllerInput(octaveInputId, progressionSelectId);
+        //initialize handlers for Pad interaction
+        this.#initializePadInput(padContainerId);
+    }
+    //initialize handlers for Controller Elements
+    static #intializeControllerInput(octaveInputId, progressionSelectId) {
+        const octaveInput = document.getElementById(octaveInputId);
+        const select = document.getElementById(progressionSelectId);
+
+        octaveInput.addEventListener('change', (event) => {
+            this.#setOctave(event.currentTarget.value);
+        });
+
+        select.addEventListener('change', (event) => {
+            this.ProgressionPads[event.target.value].renderComponent();
+        });
+        
+        this.#setOctave(octaveInput.value); //set octave value
+    }
+
+    //initialize handlers for Pad interaction
+    static #initializePadInput(containerId) {
+        const container = document.getElementById(containerId);
+        container.addEventListener('click', (event) => {
+            if (event.target.getAttribute('data-root')) {
+                console.log(event.target);
+                // let {root, quality} = this.chordGenerator.getRootQuality(event.target);
+                // console.log("Root: ", root, " Quality: ", quality);
+                // let chord = this.chordGenerator.getChord({root, quality});
+                let chord = this.chordGenerator.getChord(this.chordGenerator.getRootQuality(event.target));
+                console.log(chord);
+                chord = chord.map(note => note + 12 * this.octave);
+                console.log(chord);
+                this.#play(chord);
+                setTimeout(() => { this.#stop(chord) }, 1000);
+            }
+        });
+    }
+
+    static #renderComponents() {
+        this.ProgressionPads.major.renderComponent();
+    }
+
+    static #setOctave(newOctave) {
+        this.octave = newOctave;
+    }
+
+    static #setVelocity(newVelocity) {
+        this.velocity = newVelocity;
+    }
+
+    static #play(chord) {
+        if (!Array.isArray(chord)) {
+            throw new Error('Chord must be of type "object" to be playable : class Initializer{ static play(chord) }');
+        }
+        Array.from(chord).forEach(note => this.transmitter.play(note));
+    }
+
+    static #stop(chord) {
+        if (!Array.isArray(chord)) {
+            throw new Error('Chord must be of type "object" to be playable : class Initializer{ static stop(chord) }');
+        }
+        Array.from(chord).forEach(note => this.transmitter.stop(note));
+    }
+}
+
+const app = new Initializer("padTable", "octave", "progression");
--- init.js
+++ init.js
@@ -1,34 +1,17 @@
-const NOTE = {
-    "C": 0,
-    "C#": 1,
-    "D": 2,
-    "D#": 3,
-    "E": 4,
-    "F": 5,
-    "F#": 6,
-    "G": 7,
-    "G#": 8,
-    "A": 9,
-    "A#": 10,
-    "B": 11
-}; // midi value = NOTE["C"]+12*octave
-
-// returns notes of a chord based on root and quality
-const chord = (root, quality) => {
-    const semitones = 12;  // 12 ko 15 se replace krdo agar next octave ke notes consider krne hai for a chord
-    root = root.toUpperCase();
-    quality = quality.toLowerCase();
-    switch (quality) {
-        case "major":
-            return [NOTE[root], (NOTE[root] + 4) % semitones, (NOTE[root] + 7) % semitones];
-        case "minor":
-            return [NOTE[root], (NOTE[root] + 3) % semitones, (NOTE[root] + 7) % semitones];
-        case "diminshed":
-            return [];
-        default:
-            return [];
-    }
-}
+// const NOTE = {
+//     "C": 0,
+//     "C#": 1,
+//     "D": 2,
+//     "D#": 3,
+//     "E": 4,
+//     "F": 5,
+//     "F#": 6,
+//     "G": 7,
+//     "G#": 8,
+//     "A": 9,
+//     "A#": 10,
+//     "B": 11
+// }; // midi value = NOTE["C"]+12*octave
 
 const majorChordProg = {
     // sequence of chord qualities in the progression
@@ -45,84 +28,18 @@ const majorChordProg = {
     ]
 }
 
-// const stringToElement = (html) => {
-//     const template = document.createElement('template');
-//     console.log(html.trim());
-//     template.innerHTML = html.trim();
-//     console.log(template.content.firstElementChild);
-//     return template.content.firstElementChild;
-// }
-
-// const renderComponent = (containerClass, component, data) => {
-//     const container = document.getElementsByClassName(`${containerClass}`)[0];
-//     container.innerHTML = '';
-//     container.appendChild(stringToElement(component(data)));
-// }
-
-// const components = {
-//     pads: (progression) => {
-//         let headerRow = progression.headers.reduce((acc, curr) => acc + `<th data-quality="${curr}">${curr}</th>`, '');
-//         let rows = progression.rows.reduce((acc, ele) => {
-//             let rowData = ele.reduce((acc, curr) => acc + `<td data-root="${curr}">${curr}</td>`, '');
-//             let temp = `<tr>${rowData}</tr>`;
-//             return acc + temp;
-//         }, '');
-//         return `<table class="pad-table"><thead>${headerRow}</thead><tbody>${rows}</tbody></table>`;
-//     }
-// }
-
-// renderComponent('pads', components.pads, majorChordProg);
-
-// // console.log(chord("C", "major"));
-// class Component {
-//     // static html;
-
-//     stringToElement() {
-//         let html = Component._html;
-//         const template = document.createElement('template');
-//         console.log(html.trim());
-//         template.innerHTML = html.trim();
-//         console.log(template.content.firstElementChild);
-//         return template.content.firstElementChild;
-//     }
-
-//     renderComponent(containerClass, component, data) {
-//         const container = document.getElementsByClassName(`${containerClass}`)[0];
-//         container.innerHTML = '';
-//         container.appendChild(stringToElement(component(data)));
-//     }
-// }
-
-// class Pads extends Component {
-//     static _html;
-//     static _progression;
-
-//     set progression({ headers, rows }) {
-//         this.progression = {
-//             headers,
-//             rows
-//         }
-//     }
-
-//     set html(html) {
-//         this.html = html;
-//     }
-
-//     constructor({ headers, rows }) {
-//         this._progression = { headers, rows };
-//         this._html = this.#characteristics(this._progression);
-//     }
-
-//     #characteristics(progression) {
-//         let headerRow = progression.headers.reduce((acc, curr) => acc + `<th data-quality="${curr}">${curr}</th>`, '');
-//         let rows = progression.rows.reduce((acc, ele) => {
-//             let rowData = ele.reduce((acc, curr) => acc + `<td data-root="${curr}">${curr}</td>`, '');
-//             let temp = `<tr>${rowData}</tr>`;
-//             return acc + temp;
-//         }, '');
-//         return `<table class="pad-table"><thead>${headerRow}</thead><tbody>${rows}</tbody></table>`;
-//     }
-// }
+const minorChordProg = {
+    headers: ["Minor", "Diminished", "Major", "Minor", "Minor", "Major", "Major"],
+    rows: [
+        ["A", "B", "C", "D", "E", "F", "G"],
+        ["B", "C#", "D", "E", "F#", "G", "A"],
+        ["C", "D", "Eb", "F", "G", "Ab", "Bb"],
+        ["D", "E", "F", "G", "A", "Bb", "C"],
+        ["E", "F#", "G", "A", "B", "C", "D"],
+        ["F", "G", "Ab", "Bb", "C", "Db", "Eb"],
+        ["G", "A", "Bb", "C", "D", "Eb", "F"]
+    ]
+}
 
 class Component {
     constructor(props = {}) {
@@ -135,13 +52,13 @@ class Component {
     }
 
     renderComponent() {
-        if (!this.props.className) {
-            throw new Error("Component requires a className prop");
+        if (!this.props.containerClass) {
+            throw new Error("Component requires a containerClass prop");
         }
 
-        const container = document.getElementsByClassName(`${this.props.className}`)[0];
+        const container = document.getElementsByClassName(`${this.props.containerClass}`)[0];
         if (!container) {
-            throw new Error(`Container with class '${this.props.className}' not found`);
+            throw new Error(`Container with class '${this.props.containerClass}' not found`);
         }
         container.innerHTML = '';
         container.appendChild(this._stringToElement(this._html));
@@ -149,16 +66,14 @@ class Component {
 
     _stringToElement(html) {
         const template = document.createElement('template');
-        console.log(html.trim());
         template.innerHTML = html.trim();
-        console.log(template.content.firstElementChild);
         return template.content.firstElementChild;
     }
 }
 
 class Pad extends Component {
     getTemplate(props) {
-        const { className, progression } = props;
+        const { progression } = props;
         if (!progression) {
             throw new Error("Component requires a progression in prop");
         }
@@ -169,6 +84,6 @@ class Pad extends Component {
             let temp = `<tr>${rowData}</tr>`;
             return acc + temp;
         }, '');
-        return `<table class="${className}"><thead>${headerRow}</thead><tbody>${rows}</tbody></table>`;
+        return `<table class=pad-table><thead>${headerRow}</thead><tbody>${rows}</tbody></table>`;
     }
 }
